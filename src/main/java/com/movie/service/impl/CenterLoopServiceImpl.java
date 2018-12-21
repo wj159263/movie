cd95d2ed09681055f9490ca0e7d8fd5d36d30459
wj159263 <122318916@qq.com>
2018-12-22 00:21:11 +0800

    添加redis

--- src/main/java/com/movie/service/impl/CenterLoopServiceImpl.java
+++ src/main/java/com/movie/service/impl/CenterLoopServiceImpl.java
@@ -1,5 +1,7 @@
 package com.movie.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.movie.dao.RedisDao;
 import com.movie.dto.EyUIGridResult;
 import com.movie.dto.EyuiDatagridLoop;
 import com.movie.dto.VideoResult;
@@ -8,6 +10,8 @@ import com.movie.mapper.CenterLoopMapper;
 import com.movie.pojo.CenterLoop;
 import com.movie.pojo.CenterLoopExample;
 import com.movie.service.CenterLoopService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -18,8 +22,12 @@ import java.util.List;
 @Service
 public class CenterLoopServiceImpl implements CenterLoopService{
 
+    public static  final Logger LOGGER = LoggerFactory.getLogger(com.movie.service.impl.CenterLoopServiceImpl.class);
+
     @Autowired
     private CenterLoopMapper centerLoopMapper;
+    @Autowired
+    RedisDao redisDao;
 
     @Override
     @Transactional(propagation = Propagation.REQUIRED,readOnly = false,rollbackFor = Exception.class)
@@ -35,11 +43,26 @@ public class CenterLoopServiceImpl implements CenterLoopService{
                 throw new Exception("轮播图添加数据错误");
             }
         }
+        //轮播图数据改变则要删除redis中的轮播图数据
+        redisDao.hdel("video:", "center");
+
+
         return VideoResult.build(VideoEnum.SUCCESS,ids.length);
     }
 
     @Override
     public EyUIGridResult selectList(int page, int rows) throws Exception {
+        //向redis中查询轮播图数据，查到数据则return
+        try{
+            EyUIGridResult data =  redisDao.getCenterData();
+            if (data != null){
+                return data;
+            }
+        }catch (Exception e){
+            LOGGER.error(e.getMessage());
+        }
+
+        //若redis中查不到数据，则取数据库查
         if(page < 1){
             page = 1;
         }
@@ -52,6 +75,13 @@ public class CenterLoopServiceImpl implements CenterLoopService{
         EyUIGridResult eyUIGridResult = new EyUIGridResult();
         eyUIGridResult.setRows(loops);
         eyUIGridResult.setTotal(i);
+        //数据查完，则同步到redis
+        try{
+           String data = new ObjectMapper().writeValueAsString(eyUIGridResult);
+            redisDao.synchronizeCenter(data);
+        }catch (Exception e){
+            LOGGER.error(e.getMessage());
+        }
         return eyUIGridResult;
     }
 
@@ -70,6 +100,8 @@ public class CenterLoopServiceImpl implements CenterLoopService{
                 }
             }
         }
+        //轮播图数据改变则要删除redis中的轮播图数据
+        redisDao.hdel("video:", "center");
         return VideoResult.build(VideoEnum.SUCCESS, ids.length);
     }
 
